e488d3c5ae5184ca0929a48b8cc85c97fcdf786d
irakli iakobishvili <irakli.iakobishvili@gmail.com>
2019-03-13 22:33:06 +0400

    Route Helpers (Joi.js)

---
+++ server/helpers/routeHelpers.js
@@ -0,0 +1,128 @@
+const Joi = require("joi");
+
+const emailAndPassword = {
+  email: Joi.string()
+    .email({ minDomainAtoms: 2 })
+    .required()
+    .error(errors => {
+      errors.forEach(err => {
+        switch (err.type) {
+          case "any.empty":
+            err.message = "Email should not be empty!";
+            break;
+          case "string.email":
+            err.message = "Email should be valid!";
+            break;
+          default:
+            break;
+        }
+      });
+      return errors;
+    }),
+  password: Joi.string()
+    .min(6)
+    .max(15)
+    .required()
+    .error(errors => {
+      errors.forEach(err => {
+        switch (err.type) {
+          case "any.empty":
+            err.message = "Password should not be empty!";
+            break;
+          case "string.min":
+            err.message = `Password should have at least ${
+              err.context.limit
+            } characters!`;
+            break;
+          case "string.max":
+            err.message = `Password should have at most ${
+              err.context.limit
+            } characters!`;
+            break;
+          default:
+            break;
+        }
+      });
+      return errors;
+    })
+};
+
+module.exports = {
+  validateBody: schema => {
+    return (req, res, next) => {
+      const result = Joi.validate(req.body, schema);
+      if (result.error) {
+        return res.status(400).json(result.error.details[0].message);
+        // return res.status(400).json({error: result.error.details[0].message});
+      }
+
+      if (!req.value) {
+        req.value = {};
+      }
+      req.value["body"] = result.value;
+      next();
+    };
+  },
+
+  schemas: {
+    registerSchema: Joi.object().keys({
+      firstName: Joi.string()
+        .min(1)
+        .max(15)
+        .required(),
+      lastName: Joi.string()
+        .min(1)
+        .max(20)
+        .required(),
+      ...emailAndPassword,
+      age: Joi.number()
+        .required()
+        .error(errors => {
+          errors.forEach(err => {
+            switch (err.type) {
+              case "any.empty":
+                err.message = "Age should not be empty!"; // Remove this case
+                break;
+              case "number.base":
+                err.message = "Age should be valid!";
+                break;
+              default:
+                break;
+            }
+          });
+          return errors;
+        }),
+      birthday: Joi.number()
+        .required()
+        .error(errors => {
+          errors.forEach(err => {
+            switch (err.type) {
+              case "number.base":
+                err.message = "Birthday should be valid!";
+                break;
+              default:
+                break;
+            }
+          });
+          return errors;
+        }),
+      phone: Joi.string()
+        .required()
+        .error(errors => {
+          errors.forEach(err => {
+            switch (err.type) {
+              case "any.empty":
+                err.message = "Phone should not be empty!";
+                break;
+              default:
+                break;
+            }
+          });
+          return errors;
+        })
+    }),
+    loginSchema: Joi.object().keys({
+      ...emailAndPassword
+    })
+  }
+};
