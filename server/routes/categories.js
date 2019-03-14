127440d3851ff31675e8472942896e0d407369eb
khatia <khatiaakhalkatsishvili@gmail.com>
2019-03-15 01:12:48 +0400

    Categories Routes

---
+++ server/routes/categories.js
@@ -0,0 +1,17 @@
+const router = require("express-promise-router")();
+const CategoryController = require("../controllers/categories");
+
+const { validateBody, schemas } = require("../helpers/routeHelpers");
+
+router
+  .route("/")
+  .get(CategoryController.findAll)
+  .post(validateBody(schemas.categorySchema), CategoryController.create);
+
+router
+  .route("/:id")
+  .get(CategoryController.findByCatId)
+  .put(CategoryController.update)
+  .delete(CategoryController.remove);
+
+module.exports = router;
