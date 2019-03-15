b8d65a924b854a77d92ce3c0dfd59ca566e022e9
irakli iakobishvili <irakli.iakobishvili@gmail.com>
2019-03-15 04:04:09 +0400

    Products Controllers

---
+++ server/controllers/products.js
@@ -0,0 +1,33 @@
+const Products = require("../models/product");
+
+module.exports = {
+  create: (req, res, next) => {
+    Products.create(req.body)
+      .then(product => res.json(product))
+      .catch(() => res.status(422).json({ message: "Can't create" }));
+  },
+
+  findAll: (req, res, next) => {
+    Products.find(req.query)
+      .sort({ date: -1 })
+      .then(all => res.json(all))
+      .catch(() => res.status(422).json({ message: "Empty List" }));
+  },
+  findById: function(req, res, next) {
+    Products.findById(req.params.id)
+      .then(product => res.json(product))
+      .catch(() => res.status(422).json({ message: "Can't Find Product" }));
+  },
+  update: (req, res, next) => {
+    Products.findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(product => res.json(product))
+      .catch(() => res.status(422).json({ message: "Can't Update Product" }));
+  },
+
+  remove: (req, res, next) => {
+    Products.findById({ _id: req.params.id })
+      .then(product => product.remove())
+      .then(product => res.json(product))
+      .catch(err => res.status(422).json(err));
+  }
+};
