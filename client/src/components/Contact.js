55cde8ace03983e05733914dc484319204245c34
khatia <khatiaakhalkatsishvili@gmail.com>
2019-03-19 16:06:09 +0400

    contact page, react + css

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -5,28 +5,33 @@ export default class Contact extends Component {
   render() {
     return (
       <div className="contact-page">
-        <div className="container">
-          <form>
-            <label>
-              firstName:
-              <input type="text" name="firstName" />
-            </label>
+        <div className="contact-container">
+          <div className="contact-content">
+          <h2 className="contact-us">Contact Us</h2>
+            <form>
+              <label>
+                firstName:
+                <input type="text" name="firstName" />
+              </label>
 
-            <label>
-              lastName:
-              <input type="text" name="lastName" />
-            </label>
+              <label>
+                lastName:
+                <input type="text" name="lastName" />
+              </label>
 
-            <label>
-              email:
-              <input type="text" name="email" />
-            </label>
+              <label>
+                email:
+                <input type="text" name="email" />
+              </label>
 
-            <label>
-              input text:
-              <input type="text" name="text" />
-            </label>
-          </form>
+              <label>
+                input text:
+                <input type="text" name="text" />
+              </label>
+
+              <button className="contact-btn" type="submit">Send Your Message</button>
+            </form>
+          </div>
         </div>
       </div>
     );
