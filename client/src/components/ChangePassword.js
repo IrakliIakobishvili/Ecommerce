7179f366ff2f2a48e0c8e75ced89ee5aa13c5699
khatia <khatiaakhalkatsishvili@gmail.com>
2019-03-25 21:19:45 +0400

    Change password page

--- client/src/components/ChangePassword.js
+++ client/src/components/ChangePassword.js
@@ -1,7 +1,53 @@
 import React, { Component } from "react";
+import "../styles/changePassword.css";
 
 export default class ChangePassword extends Component {
   render() {
-    return <div>hallo</div>;
+    return (
+      <div className="chForm-conteiner">
+        <div className="form-change-password">
+          <h2>Change password</h2>
+          <span className="chPassword-span">
+            It's a good idea to use a strong password that you're not using
+            elsewhere
+          </span>
+          <form className="">
+            <input
+              name="currentPassword"
+              type="password"
+              placeholder="Current Password"
+              required="required"
+              className="form-control"
+            />
+
+            <input
+              name="newPassword"
+              type="password"
+              placeholder="New Password"
+              required="required"
+              className="form-control"
+            />
+
+            <input
+              name="newPasswordRepeated"
+              type="password"
+              placeholder="New Password Repeated"
+              required="required"
+              className="form-control"
+            />
+
+            <button
+              type="submit"
+              size="lg"
+              block
+              color="success"
+              className="chBTN"
+            >
+              Save Changes
+            </button>
+          </form>
+        </div>
+      </div>
+    );
   }
 }
