d205fb3b5910ed93c773388772dea7c697e60413
khatia <khatiaakhalkatsishvili@gmail.com>
2019-03-29 19:53:31 +0400

    Contact Form Validation (Client)

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,65 +1,99 @@
 import React, { Component } from "react";
 import "../styles/contact.css";
 
+const initialState = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  message: ""
+};
 export default class Contact extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      firstname: "",
-      lastname: "",
-      email: ""
-    };
-  }
-
-  handleNameChange = event => {
-    this.setState({ firstname: event.target.value }, () => {
-      this.validateName();
-    });
+  state = {
+    ...initialState
   };
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     firstname: "",
+  //     lastname: "",
+  //     email: ""
+  //   };
+  // }
 
-  handleLastnameChange = event => {
-    this.setState({ lastname: event.target.value }, () => {
-      this.validateName();
-    });
-  };
+  // handleNameChange = event => {
+  //   this.setState({ firstname: event.target.value }, () => {
+  //     this.validateName();
+  //   });
+  // };
 
-  handleEmailChange = event => {
-    this.setState({ email: event.target.value }, () => {
-      this.validateEmail();
-    });
-  };
+  // handleLastnameChange = event => {
+  //   this.setState({ lastname: event.target.value }, () => {
+  //     this.validateName();
+  //   });
+  // };
 
-  validateName = () => {
-    const { firstname } = this.state;
-    this.setState({
-      nameError:
-        firstname.length > 1 ? null : " "
-    });
-  };
+  // handleEmailChange = event => {
+  //   this.setState({ email: event.target.value }, () => {
+  //     this.validateEmail();
+  //   });
+  // };
 
-  validateLastname = () => {
-    const { lastname } = this.state;
-    this.setState({
-      lastnameError:
-        lastname.length > 1 ? null : " "
-    });
-  };
+  // validateName = () => {
+  //   const { firstname } = this.state;
+  //   this.setState({
+  //     nameError:
+  //       firstname.length > 1 ? null : " "
+  //   });
+  // };
 
-  validateEmail = () => {
-    const { email } = this.state;
-    this.setState({
-      emailError:
-        email.length > 7 ? null : " "
-    });
-  };
+  // validateLastname = () => {
+  //   const { lastname } = this.state;
+  //   this.setState({
+  //     lastnameError:
+  //       lastname.length > 1 ? null : " "
+  //   });
+  // };
 
+  // validateEmail = () => {
+  //   const { email } = this.state;
+  //   this.setState({
+  //     emailError:
+  //       email.length > 7 ? null : " "
+  //   });
+  // };
+
+  // handleSubmit = event => {
+  //   event.preventDefault();
+  //   const { firstname, lastname, email } = this.state;
+  //   alert(`Your state values: \n
+  //           firstname: ${firstname} \n
+  //           lastname: ${lastname} \n
+  //           email: ${email}`);
+  // };
   handleSubmit = event => {
     event.preventDefault();
-    const { firstname, lastname, email } = this.state;
-    alert(`Your state values: \n 
-            firstname: ${firstname} \n 
-            lastname: ${lastname} \n 
-            email: ${email}`);
+    console.log(this.state);
+    const keys = Object.keys(this.state);
+    const values = Object.values(this.state);
+
+    let emptyFields = 0;
+    keys.forEach((el, i) => {
+      if (values[i] === "") {
+        console.log(el + " is Empty!");
+        emptyFields++;
+      }
+    });
+    if (emptyFields) {
+      console.log("Fill Al Field");
+    } else {
+      console.log("Send To DB");
+    }
+  };
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  resetState = () => {
+    this.setState(initialState);
   };
 
   render() {
@@ -71,39 +105,46 @@ export default class Contact extends Component {
 
             <form onSubmit={this.handleSubmit}>
               <div className="form-group">
-                <label htmlFor="name" className="contact-label">
+                <label htmlFor="firstName" className="contact-label">
                   Firstname
                 </label>
                 <input
-                  name="name"
+                  name="firstName"
                   className={`contact-input ${
-                    this.state.nameError ? "is-invalid" : ""
+                    this.state.firstName ? "is-valid" : ""
                   }`}
-                  id="name"
-                  value={this.state.firstname}
-                  onChange={this.handleNameChange}
-                  onBlur={this.validateName}
+                  // className={`contact-input ${
+                  //   this.state.nameError ? "is-invalid" : ""
+                  // }`}
+                  id="firstName"
+                  value={this.state.firstName}
+                  onChange={this.handleChange}
+                  // onBlur={this.validateName}
                 />
-                <div className="invalid-feedback">{this.state.nameError}</div>
+                {/* <div className="invalid-feedback">{this.state.nameError}</div> */}
               </div>
 
               <div className="form-group">
-                <label htmlFor="lastname" className="contact-label">
+                <label htmlFor="lastName" className="contact-label">
                   Lastname
                 </label>
                 <input
-                  name="lastname"
+                  name="lastName"
+                  // className={`contact-input`}
                   className={`contact-input ${
-                    this.state.lastnameError ? "is-invalid" : ""
+                    this.state.lastName ? "is-valid" : ""
                   }`}
-                  id="lastname"
-                  value={this.state.lastname}
-                  onChange={this.handleLastnameChange}
-                  onBlur={this.validateLastname}
+                  // className={`contact-input ${
+                  //   this.state.lastnameError ? "is-invalid" : ""
+                  // }`}
+                  // id="lastname"
+                  value={this.state.lastName}
+                  onChange={this.handleChange}
+                  // onBlur={this.validateLastname}
                 />
-                <div className="invalid-feedback">
+                {/* <div className="invalid-feedback">
                   {this.state.lastnameError}
-                </div>
+                </div> */}
               </div>
 
               <div className="form-group">
@@ -113,19 +154,39 @@ export default class Contact extends Component {
                 <input
                   name="email"
                   className={`contact-input ${
-                    this.state.emailError ? "is-invalid" : ""
+                    this.state.email ? "is-valid" : ""
                   }`}
-                  id="email"
+                  // className={`contact-input ${
+                  //   this.state.emailError ? "is-invalid" : ""
+                  // }`}
+                  // id="email"
                   value={this.state.email}
-                  onChange={this.handleEmailChange}
-                  onBlur={this.validateEmail}
+                  onChange={this.handleChange}
+                  // onBlur={this.validateEmail}
                 />
-                <div className="invalid-feedback">{this.state.emailError}</div>
+                {/* <div className="invalid-feedback">{this.state.emailError}</div> */}
               </div>
 
               <label className="contact-label">
-                Input text:
-                <input type="textarea" name="text" className="contact-input" />
+                {/* Input text: */}
+                {/* <input
+                  value={this.state.message}
+                  onChange={this.handleChange}
+                  type="textarea"
+                  name="message"
+                  className="contact-input"
+                /> */}
+                <textarea
+                  onChange={this.handleChange}
+                  style={{ resize: "none" }}
+                  value={this.state.message}
+                  // value={}
+                  // onChange={this.handleChange}
+                  className={`contact-input ${
+                    this.state.message ? "is-valid" : ""
+                  }`}
+                  name="message"
+                />
               </label>
               <button type="submit" className="contact-btn">
                 Send Your Message
