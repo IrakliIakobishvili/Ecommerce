ad74de7a59e155d400092fb41a7054d19abcc198
irakli iakobishvili <irakli.iakobishvili@gmail.com>
2019-03-31 16:56:24 +0400

    Active Link Detection (Admin Panel)

--- client/src/components/admin/Navigation.js
+++ client/src/components/admin/Navigation.js
@@ -3,28 +3,61 @@ import { Link } from "react-router-dom";
 import "../../styles/admin/navigation.css";
 
 export default class Navigation extends Component {
+  state = {
+    activeLink: ""
+  };
+
+  activeLinkHandler = async className => {
+    await this.setState({ activeLink: className });
+    console.log(this.state.activeLink);
+  };
   render() {
     return (
       <nav className="admin-page-nav">
         <h3 className="admin-page-nav__heading ">SETTINGS</h3>
         <ul>
           <li className="admin-list__item">
-            <Link className="admin-list__item__link" to="/admin/users">
+            <Link
+              onClick={() => this.activeLinkHandler("users")}
+              className={`admin-list__item__link ${
+                this.state.activeLink == "users" ? "active-admin" : ""
+              }`}
+              to="/admin/users"
+            >
               Users
             </Link>
           </li>
           <li className="admin-list__item">
-            <Link className="admin-list__item__link" to="/admin/products">
+            <Link
+              onClick={() => this.activeLinkHandler("products")}
+              className={`admin-list__item__link ${
+                this.state.activeLink == "products" ? "active-admin" : ""
+              }`}
+              // className="admin-list__item__link"
+              to="/admin/products"
+            >
               Products
             </Link>
           </li>
           <li className="admin-list__item">
-            <Link className="admin-list__item__link" to="/admin/categories">
+            <Link
+              onClick={() => this.activeLinkHandler("categories")}
+              className={`admin-list__item__link ${
+                this.state.activeLink == "categories" ? "active-admin" : ""
+              }`}
+              to="/admin/categories"
+            >
               Categories
             </Link>
           </li>
           <li className="admin-list__item">
-            <Link className="admin-list__item__link" to="/admin/contacts">
+            <Link
+              onClick={() => this.activeLinkHandler("contacts")}
+              className={`admin-list__item__link ${
+                this.state.activeLink == "contacts" ? "active-admin" : ""
+              }`}
+              to="/admin/contacts"
+            >
               Contacts
             </Link>
           </li>
