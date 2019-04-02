5bf74af855ba3035df1448d7f3bd019c70b985bc
khatia <khatiaakhalkatsishvili@gmail.com>
2019-04-03 03:24:15 +0400

    Chat Actions

---
+++ client/src/actions/chat.js
@@ -0,0 +1,57 @@
+import axios from "axios";
+import { API_URL } from "../config";
+import { SEND_MESSAGE, CONNECTION_ERROR } from "./types";
+
+export const sendMessage = message => {
+  return async dispatch => {
+    try {
+      const res = await axios.post(`${API_URL}/api/chat`, {
+        text: message
+      });
+      console.log(res.data);
+      dispatch({
+        type: SEND_MESSAGE,
+        payload: res.data
+      });
+    } catch (err) {
+      console.error("err", err);
+      dispatch({
+        type: CONNECTION_ERROR,
+        payload: true
+      });
+    }
+  };
+};
+
+export const getMessages = () => {
+  return async dispatch => {
+    try {
+      const res = await axios.get(`${API_URL}/api/chat/user`);
+      // res.data;
+      console.log(res.data);
+      //   dispatch({
+      //     type: GET_MESSAGES,
+      //     payload: res.data
+      //   });
+    } catch (err) {
+      console.error("err", err);
+      dispatch({
+        type: CONNECTION_ERROR,
+        payload: true
+      });
+    }
+  };
+};
+
+// export const clearFeedback = id => {
+//   return async dispatch => {
+//     try {
+//       dispatch({
+//         type: CONTACT_FEEDBACK_CLEAR,
+//         payload: ""
+//       });
+//     } catch (err) {
+//       console.error("err", err);
+//     }
+//   };
+// };
